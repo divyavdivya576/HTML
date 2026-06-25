f4485537c72e350b0d2bc119f04509c2c623735d
divyavdivya576 <divyavdivya576@gmail.com>
2026-06-25 15:37:27 +0530

    Completed owner request approval

--- backend/models/Rental.js
+++ backend/models/Rental.js
@@ -1,11 +1,24 @@
 const mongoose = require("mongoose");
 
 const rentalSchema = new mongoose.Schema({
+
     farmerName: String,
+
     phone: String,
+
+    email: String,
+
     duration: String,
+
     crop: String,
-    message: String
+
+    message: String,
+
+    status: {
+        type: String,
+        default: "Pending"
+    }
+
 });
 
 module.exports = mongoose.model("Rental", rentalSchema);
--- backend/routes/RentalRoutes.js
+++ backend/routes/RentalRoutes.js
@@ -5,9 +5,10 @@ const Rental = require("../models/Rental");
 
 // Save Rental Request
 router.post("/rent", async (req, res) => {
+
+
 try {
 
-```
     const rental = new Rental(req.body);
 
     await rental.save();
@@ -27,15 +28,15 @@ try {
     });
 
 }
-```
+
 
 });
 
 // Get All Rental Requests
 router.get("/rentals", async (req, res) => {
+
 try {
 
-```
     const rentals = await Rental.find();
 
     res.json(rentals);
@@ -50,7 +51,38 @@ try {
     });
 
 }
-```
+
+
+});
+
+// Update Rental Status
+router.put("/rentals/:id", async (req, res) => {
+
+
+try {
+
+    await Rental.findByIdAndUpdate(
+        req.params.id,
+        {
+            status: req.body.status
+        }
+    );
+
+    res.json({
+        success: true,
+        message: "Status Updated Successfully"
+    });
+
+} catch (error) {
+
+    console.log(error);
+
+    res.status(500).json({
+        success: false,
+        message: "Update Failed"
+    });
+
+}
 
 });
 
